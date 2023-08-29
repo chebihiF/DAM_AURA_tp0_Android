736a4dd78e6cf602a90e72126c36bf348b0ce531
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-29 11:51:54 +0100

    add colors

--- app/src/main/java/org/m2i/tp0/MainActivity.java
+++ app/src/main/java/org/m2i/tp0/MainActivity.java
@@ -3,12 +3,28 @@ package org.m2i.tp0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1 - déclaration (UI)
+    TextView t_hello;
+    Button btn_valider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 2 - relation via R
+        t_hello = findViewById(R.id.t_hello);
+        btn_valider = findViewById(R.id.btn_valider);
+
+        // 3 - manipuler (UI)
+        t_hello.setText("Hello from java file");
+
+
     }
 }
